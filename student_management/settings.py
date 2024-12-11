1247e940881d7f3eda3b080b586d499244aa9e40
stoyanadov <stoian671@gmail.com>
2024-12-11 21:55:56 +0200

    Updated settings.py

--- student_management/settings.py
+++ student_management/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
